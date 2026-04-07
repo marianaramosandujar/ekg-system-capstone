5e06c3d5f191dd0c0bc2684741fcbc394cf41147
mramosandujar <ramos-andujar.1@buckeyemail.osu.edu>
2026-04-07 19:57:32 -0400

    bah

--- setup_and_run_mac.sh
+++ setup_and_run_mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo "Setting up EKG System..."
 
@@ -6,6 +7,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 python3 -m pip install --upgrade pip
-pip install -r requirements.txt
+python3 -m pip install --no-compile -r requirements.txt
 
 python3 ui_main.py
