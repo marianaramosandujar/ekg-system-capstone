fe316cf4071e8b70cb623a508b4d2c57a5bac744
marianaramosandujar <97976577+marianaramosandujar@users.noreply.github.com>
2026-04-07 22:07:17 -0400

    view

--- ekg_system/clinical_pg_view.py
+++ ekg_system/clinical_pg_view.py
@@ -1,163 +1,97 @@
 import numpy as np
 import pyqtgraph as pg
-
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
-from PySide6.QtCore import QSize, Qt
-
+from PySide6.QtCore import QSize
 import qtawesome as qta
 
-
-def style_ecg_plot(plot_widget):
-    plot_widget.setBackground("w")
-
-    axis_pen = pg.mkPen(color="black", width=2)
-    plot_widget.getAxis("bottom").setPen(axis_pen)
-    plot_widget.getAxis("left").setPen(axis_pen)
-
-    plot_widget.getAxis("bottom").setTextPen("black")
-    plot_widget.getAxis("left").setTextPen("black")
-
-    plot_widget.showGrid(x=True, y=True, alpha=0.12)
-
-
 class ClinicalPGView(QWidget):
-
     def __init__(self, parent=None, signal=None, fs=1000, window_sec=30):
         super().__init__(parent)
-
-        self.signal = signal
+        
+        print("Creating ClinicalPGView...")  # Debug
+        
         self.fs = fs
         self.window_sec = window_sec
-        self.max_display_points = 20000  # Limit points for performance
-
-        layout = QVBoxLayout(self)
-
-        self.plot = pg.PlotWidget()
-        layout.addWidget(self.plot)
-
-        self.plot.setMouseEnabled(x=True, y=False)
-        self.plot.setLabel("bottom", "Time (s)")
-        self.plot.setLabel("left", "Amplitude (mV)")
-        style_ecg_plot(self.plot)
-        
-        # Safe viewport update mode
-        try:
-            self.plot.setViewportUpdateMode(pg.ViewportUpdateMode.FullViewportUpdate)
-        except AttributeError:
-            try:
-                self.plot.setViewportUpdateMode(3)
-            except:
-                pass
-
-        # Downsample signal for display
-        display_signal, display_t = self._downsample_for_display(signal)
-        
-        self.curve = self.plot.plot(
-            display_t,
-            display_signal,
-            pen=pg.mkPen(color="black", width=1.5)
-        )
-
-        self.sig_min = float(np.min(display_signal))
-        self.sig_max = float(np.max(display_signal))
-        self.plot.setYRange(self.sig_min, self.sig_max)
-
         self.duration = len(signal) / fs
-        end = min(self.window_sec, self.duration)
+        
+        layout = QVBoxLayout(self)
+        
+        # Create plot
+        self.plot = pg.PlotWidget()
+        self.plot.setBackground("white")
+        layout.addWidget(self.plot)
+        
+        # Downsample
+        step = max(1, len(signal) // 20000)
+        t = np.arange(0, len(signal), step) / fs
+        y = signal[::step]
+        
+        # Plot
+        self.plot.plot(t, y, pen=pg.mkPen(color="black", width=1.5))
+        
+        # Set initial view
+        end = min(window_sec, self.duration)
         self.plot.setXRange(0, end)
-
-        self.plot.sigRangeChanged.connect(self._fix_bounds)
-
-        nav_layout = QHBoxLayout()
-        layout.addLayout(nav_layout)
-
-        nav_layout.addStretch()  # keeps buttons grouped
-
-        btn_defs = [
-            ("fa5s.backward", "Start", self.go_to_start),
+        self.plot.setYRange(float(np.min(y)), float(np.max(y)))
+        
+        # Buttons
+        btn_layout = QHBoxLayout()
+        layout.addLayout(btn_layout)
+        
+        for icon, tip, func in [
+            ("fa5s.backward", "Start", self.go_start),
             ("fa5s.arrow-left", "Left", self.pan_left),
             ("fa5s.search-minus", "Zoom Out", self.zoom_out),
-            ("fa5s.sync", "Reset", self.reset_view),
+            ("fa5s.sync", "Reset", self.reset),
             ("fa5s.search-plus", "Zoom In", self.zoom_in),
             ("fa5s.arrow-right", "Right", self.pan_right),
-            ("fa5s.forward", "End", self.go_to_end),
-        ]
-
-        for icon_name, tooltip, handler in btn_defs:
+            ("fa5s.forward", "End", self.go_end),
+        ]:
             btn = QPushButton()
-            btn.setIcon(qta.icon(icon_name))
+            btn.setIcon(qta.icon(icon))
             btn.setIconSize(QSize(20, 20))
-            btn.setToolTip(tooltip)
+            btn.setToolTip(tip)
             btn.setFixedSize(42, 40)
-            btn.clicked.connect(handler)
-            nav_layout.addWidget(btn)
-
-        nav_layout.addStretch()
-
-        self.plot.scene().sigMouseClicked.connect(self._check_double_click)
-
-    def _downsample_for_display(self, signal):
-        """Downsample signal to max_display_points for faster rendering"""
-        if len(signal) <= self.max_display_points:
-            t = np.arange(len(signal)) / self.fs
-            return signal, t
-        
-        step = len(signal) // self.max_display_points
-        downsampled = signal[::step]
-        t = np.arange(len(downsampled)) * step / self.fs
-        return downsampled, t
-
-    def _fix_bounds(self, xmin=None, xmax=None):
-        if xmin is None:
-            xmin, xmax = self.plot.viewRange()[0]
-
-        if xmin < 0:
-            xmin = 0
-
-        if xmax > self.duration:
-            xmax = self.duration
-
-        if xmax - xmin < 0.05:
-            xmax = xmin + 0.05
-
-        self.plot.blockSignals(True)
-        self.plot.setXRange(xmin, xmax, padding=0)
-        self.plot.setYRange(self.sig_min, self.sig_max)
-        self.plot.blockSignals(False)
-
-    def reset_view(self):
-        end = min(self.window_sec, self.duration)
-        self.plot.setXRange(0, end)
-        self.plot.setYRange(self.sig_min, self.sig_max)
-
+            btn.clicked.connect(func)
+            btn_layout.addWidget(btn)
+    
+    def get_range(self):
+        return self.plot.viewRange()[0]
+    
+    def set_range(self, x1, x2):
+        x1 = max(0, float(x1))
+        x2 = min(self.duration, float(x2))
+        if x2 - x1 < 0.01:
+            x2 = x1 + 0.01
+        self.plot.setXRange(x1, x2, padding=0)
+    
+    def reset(self):
+        self.set_range(0, min(self.window_sec, self.duration))
+    
     def zoom_in(self):
-        xmin, xmax = self.plot.viewRange()[0]
-        mid = (xmin + xmax) / 2
-        span = (xmax - xmin) * 0.8
-        self._fix_bounds(mid - span/2, mid + span/2)
-
+        x1, x2 = self.get_range()
+        c = (x1 + x2) / 2
+        w = (x2 - x1) * 0.8
+        self.set_range(c - w/2, c + w/2)
+    
     def zoom_out(self):
-        xmin, xmax = self.plot.viewRange()[0]
-        mid = (xmin + xmax) / 2
-        span = (xmax - xmin) * 1.25
-        self._fix_bounds(mid - span/2, mid + span/2)
-
+        x1, x2 = self.get_range()
+        c = (x1 + x2) / 2
+        w = (x2 - x1) * 1.25
+        self.set_range(c - w/2, c + w/2)
+    
     def pan_left(self):
-        xmin, xmax = self.plot.viewRange()[0]
-        shift = (xmax - xmin) * 0.2
-        self._fix_bounds(xmin - shift, xmax - shift)
-
+        x1, x2 = self.get_range()
+        w = x2 - x1
+        self.set_range(x1 - w*0.2, x2 - w*0.2)
+    
     def pan_right(self):
-        xmin, xmax = self.plot.viewRange()[0]
-        shift = (xmax - xmin) * 0.2
-        self._fix_bounds(xmin + shift, xmax + shift)
-
-    def go_to_start(self):
-        self._fix_bounds(0, self.window_sec)
-
-    def go_to_end(self):
-        self._fix_bounds(max(0, self.duration - self.window_sec), self.duration)
-
-    def _check_double_click(self, event):
-        if event.double():
-            self.reset_view()
+        x1, x2 = self.get_range()
+        w = x2 - x1
+        self.set_range(x1 + w*0.2, x2 + w*0.2)
+    
+    def go_start(self):
+        self.set_range(0, min(self.window_sec, self.duration))
+    
+    def go_end(self):
+        self.set_range(max(0, self.duration - self.window_sec), self.duration)
